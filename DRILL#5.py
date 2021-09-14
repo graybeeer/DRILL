2b0724f98a50222ee7bd38a407bda2194ec92fc0
MoonSeongChan <sunim555@naver.com>
2021-09-14 18:31:11 +0900

    Create DRILL#5.py
    유튜브 실행 영상
    
    https://youtu.be/U6m4rDwKz_8

---
+++ DRILL#5.py
@@ -0,0 +1,32 @@
+import turtle
+
+def w_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def a_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def s_move():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def d_move():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+turtle.onkey(w_move,'W')
+turtle.onkey(a_move,'A')
+turtle.onkey(s_move,'S')
+turtle.onkey(d_move,'D')
+turtle.onkey(restart,'Escape')
+turtle.listen()
