04c5064332f8d9a75a1717cc4037e5fc90494c9e
MoonSeongChan <sunim555@naver.com>
2021-11-04 17:22:40 +0900

    Drill#11 update

--- 11/main_state.py
+++ 11/main_state.py
@@ -5,103 +5,32 @@ import os
 from pico2d import *
 
 import game_framework
-import title_state
+
+
+from boy import Boy
+from grass import Grass
+
+
 
 name = "MainState"
 
-player = None
-block = None
+boy = None
+grass = None
 font = None
 
 
-class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
-
-
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
-key_event_table = {
-    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
-    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
-    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
-    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
-}
-
-class IdleState:
-    def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.velocity += 1
-        elif event == LEFT_DOWN:
-            boy.velocity -= 1
-        elif event == RIGHT_UP:
-            boy.velocity -= 1
-        elif event == LEFT_UP:
-            boy.velocity += 1
-            boy.timer = 1000
-
-    def exit(boy, event):
-        pass
-
-    def do(boy):
-        boy.frame = (boy.frame + 1) % 8
-
-        boy.timer -= 1
-
-    def draw(boy):
-        if boy.dir == 1:
-            boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
-
-        else:
-            boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
-class Boy:
-    def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
-        self.image = load_image('run_animation.png')
-        self.dir = 1
-        self.velocity = 0
-        self.frame = 0
-        self.timer = 0
-        self.event_que = []
-        self.cur_state = 'IdleState'
-        self.cur_state.enter(self, None)
-
-    def add_event(self, event):
-        self.event_que.insert(0, event)
-
-    def update(self):
-        self.cur_state.do(self)
-
-        if len(self.event_que) > 0:
-            event = self.event_que.pop()
-        self.cur_state.exit(self, event)
-        self.cur_state = next_state_table[self.cur_state][event]
-        self.cur_state.enter(self, event)
-
-    def draw(self):
-        self.cur_state.draw(self)
-
-    def handle_event(self, event):
-        if (event.type, event.key) in key_event_table:
-            key_event = key_event_table[(event.type, event.key)]
-            self.add_event(key_event)
-
 
 def enter():
-    global player, block
-    player = Boy()
-    block = Grass()
-    pass
+    global boy, grass
+    boy = Boy()
+    grass = Grass()
 
 
 def exit():
-    global player, block
-    del (player)
-    del (block)
-    pass
+    global boy, grass
+    del boy
+    del grass
+
 
 
 def pause():
@@ -117,19 +46,24 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-            game_framework.change_state(title_state)
-    pass
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.quit()
+        else:
+            boy.handle_event(event)
+
 
 
 def update():
-    player.update()
-    pass
-
+    boy.update()
 
 def draw():
     clear_canvas()
-    block.draw()
-    player.draw()
+    grass.draw()
+    boy.draw()
     update_canvas()
-    pass
+
+
+
+
+
+
